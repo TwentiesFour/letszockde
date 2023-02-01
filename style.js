4e4b4ad80aa97d34f24453869989947ba191b6fe
TwentiesFour <herrgamerlp@gmail.com>
2023-02-02 00:01:06 +0100

    edit darkmode

--- style.js
+++ style.js
@@ -1,2 +1,26 @@
-const dark_mode_button = document.getElementById("dark_mode_button")
-dark_mode_button.addEventListener("click", () =>{document.body.classList.toggle("dark_mode")})
+let DARKMODE = false;
+const BUTTON = document.getElementById('dark_mode_button');
+
+const switchMode = () => {
+    DARKMODE = !DARKMODE;
+    setClassToBody();
+};
+
+const setClassToBody = () => {
+    if (DARKMODE) {
+        document.querySelector('body').classList.remove('light');
+        document.querySelector('body').classList.add('dark');
+    } else {
+        document.querySelector('body').classList.remove('dark');
+        document.querySelector('body').classList.add('light');
+    }
+}
+
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    DARKMODE = true;
+} else {
+    DARKMODE = false;
+}
+
+BUTTON.addEventListener('click', switchMode);
+setClassToBody();
